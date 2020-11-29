267430315c4282c686a5b471bb74e9ff6ed3daa5
licop <licop@users.noreply.github.com>
2020-11-29 11:54:41 +0800

    update

--- demo1-2/class.ts
+++ demo1-2/class.ts
@@ -27,11 +27,19 @@ console.log(person1.getName);
 // contructor new实例的时候contructor会执行
 class Person2 {
     // 简化写法
-    constructor(public name: string) {
-
+    constructor(private _name: string) {
+    }
+    // getter
+    get name() {
+        return this._name;
+    }
+    // setter
+    set name(name: string) {
+        this._name = name;
     }
 } 
 
+const person2 = new Person2('licop2');
 class Teacher2 extends Person2{
     constructor(public age: number) {
         // 子类的constuctor必须调用super()
@@ -40,5 +48,45 @@ class Teacher2 extends Person2{
 }
 
 const teacher = new Teacher2(28);
-console.log(teacher.age);
-console.log(teacher.name);
+console.log(person2.name);
+
+// 单例模式
+class Single {
+    private static instance: Single;
+    private constructor(public name: string) {
+
+    }
+    // 将getInstance挂载到Demo
+    static getInstance() {
+        if(!this.instance) {
+            this.instance = new Single('licop');
+        }
+        return this.instance;
+    }   
+}
+const demo1 = Single.getInstance();
+console.log(demo1.name)
+
+
+// 抽象类
+abstract class Geom {
+    width: number;
+    getType() {
+       return 'Gemo';
+    }
+    abstract getArea(): number;
+}
+
+class Circle extends Geom {
+    getArea() {
+        return 123;
+    }
+}
+
+
+
+
+
+
+
+
